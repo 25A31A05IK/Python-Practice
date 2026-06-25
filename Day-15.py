c340851d18a83355e29681e5c53e9ce54513062d
Balaganesh <balaganeshpasagadugula24@gmail.com>
2026-06-25 09:17:06 +0530

    Added Day-15 and Day-16 pyhton programs

---
+++ Day-15.py
@@ -0,0 +1,31 @@
+"""
+for i in range(1,11):
+    print(i)
+    if i==6:
+        break
+"""
+"""
+for i in range(1,11):
+    if i==4 or i==7:
+        continue
+    print(i)
+"""
+"""for i, fruit in enumerate(['apple','mango','banana','orange']):
+    print(fruit)
+    if fruit=='banana':
+        print(f'Banana found at index {i}')
+        break
+"""
+"""
+for i in range(1,20):
+    if i%4==0:
+        continue
+    print(i)
+"""
+def process_payment():
+    pass
+def send_email():
+    pass
+process_payment()
+send_email()
+print('Ran Successfully')
