7376ff4f6a84be7f3d04fa606ffa85283d4ed842
Balaganesh <balaganeshpasagadugula24@gmail.com>
2026-06-25 09:40:00 +0530

    Adding Day-16 Pyhton Programs

---
+++ Day-16.py
@@ -0,0 +1,35 @@
+"""
+name = input()
+def greet(name):
+    print(f'Hello {name}!Welcome to Python.')
+greet(name)
+"""
+"""
+def add(a,b):
+    return a+b
+addition=add(3,4)
+print(addition)
+def multiply(a,b):
+    return a*b
+multiplication=multiply(4,3)
+print(multiplication)
+"""
+"""
+number = int(input())
+def is_even(number):
+    if number%2==0:
+        print('Even number')
+    else:
+        print('Odd number')
+is_even(number)
+"""
+"""
+number=int(input('Enter a number: '))
+def number_table(number):
+    for i in range(1,11):
+        print(f'{number} x {i} = {number*i}')
+number_table(number)
+"""
+def full_name(first,last):
+    print(first,last)
+full_name('Pasagadugula','Balaganesh')
